8d9a001bd824729969193afc555ccaf91bde3561
gr3ger <gr3ger@gmail.com>
2020-02-17 17:47:48 +0100

    formatting

--- main.py
+++ main.py
@@ -21,9 +21,11 @@ def write_option(key, value):
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
 
+
 @bot.command()
 async def hello(ctx):
     await ctx.send("Hello {}!".format(ctx.message.author.name))
+
 
 @bot.command()
 @commands.has_any_role("Mods", "Admin")
@@ -32,34 +34,43 @@ async def enabletwitchannouncement(ctx, arg):
     if isinstance(ctx.message.channel, discord.TextChannel):
         write_option("AnnouncementChannel", ctx.message.channel.id.__str__())
         write_option("TwitchChannel", arg)
-        await ctx.send("Successfully set the announcement channel to: {}, I will post here when user {} comes online.".format(ctx.message.channel.name, arg))
+        await ctx.send("Successfully set the announcement channel to: {}, I will post here when user {} comes online."
+                       .format(ctx.message.channel.name, arg))
     else:
         await ctx.send("Needs to be done in a regular channel")
         return
 
+
 @enabletwitchannouncement.error
 async def enabletwitchannouncement_error(ctx, error):
     if isinstance(error, commands.UserInputError):
-        await ctx.send('Usage: `{}enabletwitchannouncement <twitch_channel_id>` \nIt must be used in a regular channel so it knows where to post announcements.'.format(CALL_CHARACTER))
+        await ctx.send('Usage: `{}enabletwitchannouncement <twitch_channel_id>` '
+                       '\nIt must be used in a regular channel so it knows where to post announcements.'
+                       .format(CALL_CHARACTER))
+
 
 @bot.command()
 @commands.has_any_role("Mods", "Admin")
 async def makepoll(ctx):
     await ctx.send("makepoll: Not implemented yet")
 
+
 @bot.command()
 async def addoption(ctx):
     await ctx.send("addoption: Not implemented yet")
+
 
 @bot.command()
 @commands.has_any_role("Mods", "Admin")
 async def forceaddoption(ctx):
     await ctx.send("forceaddoption: Not implemented yet")
 
+
 @bot.command()
 @commands.has_any_role("Mods", "Admin")
 async def removeoption(ctx):
     await ctx.send("forceaddoption: Not implemented yet")
+
 
 @bot.event
 async def on_ready():
