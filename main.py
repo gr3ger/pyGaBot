b6ee4bcf01d4c2e56c19c6f6938a2dd28d5385d6
jonas <jonas@thefarm.se>
2020-03-12 10:09:46 +0100

    add support for custom commands

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import asyncio
+import os
+import pickle
 import sys
 from os.path import exists
 
 import jsonpickle
 from discord.ext import commands
+from discord.ext.commands import CommandNotFound
 
 import settings
 from poll_cog import PollCog
@@ -20,20 +23,87 @@ if not exists("config.ini"):
 
 async_loop = asyncio.get_event_loop()
 bot = commands.Bot(command_prefix=settings.CALL_CHARACTER)
+custom_commands = {}
+
+if os.path.isfile('custom_commands.bin'):
+    custom_commands = pickle.load(open("custom_commands.bin", "rb"))
 
 
 @bot.command(hidden=True)
 @commands.has_any_role("Mods", "Admin")
 async def test(ctx):
     """Developer test function"""
-    # await updatePollStatus(0)
     print(jsonpickle.encode(bot.get_cog("Polls").pollList.polls))
+
 
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
     bot.add_cog(PollCog(bot))
     bot.add_cog(TwitchCog(bot))
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, CommandNotFound):
+        command = ctx.message.content.split()[0].replace(bot.command_prefix, "")
+        if command in custom_commands:
+            await ctx.send(custom_commands[command])
+        return
+    raise error
+
+
+@bot.command()
+@commands.has_any_role("Mods", "Admin")
+async def addcommand(ctx, *args):
+    """Adds a custom text command"""
+    cog_commands = []
+    for cog in bot.cogs:
+        for command in bot.get_cog(cog).get_commands():
+            cog_commands.append(command.name)
+
+    if args[0] in custom_commands:
+        await ctx.send("there is already a custom command named `{}`".format(args[0]), delete_after=3)
+        return
+    if args[0] in cog_commands:
+        await ctx.send("there is already a native command named `{}`".format(args[0]), delete_after=3)
+        return
+
+    value = " ".join(args[1:])
+
+    if value == "":
+        await ctx.send("The value of the command cannot be empty".format(args[0]), delete_after=3)
+        return
+
+    custom_commands[args[0]] = " ".join(args[1:])
+    pickle.dump(custom_commands, open("custom_commands.bin", "wb"))
+    await ctx.send("`{}` has been added as a new custom command".format(args[0]))
+
+
+@bot.command()
+@commands.has_any_role("Mods", "Admin")
+async def removecommand(ctx, key):
+    """Removes a custom text command"""
+    if key in custom_commands:
+        del custom_commands[key]
+        await ctx.send("`{}` has been removed from the custom command list".format(key))
+        pickle.dump(custom_commands, open("custom_commands.bin", "wb"))
+    else:
+        await ctx.send("there is no custom command named `{}`".format(key), delete_after=3)
+
+
+@bot.command()
+async def textcommands(ctx):
+    """Lists all custom text commands"""
+    output = "```"
+    for key in custom_commands:
+        output += "{} - {}\n".format(key, custom_commands[key])
+
+    if output != "```":
+        output += "```"
+        await ctx.send(output)
+    else:
+        await ctx.send("There are currently no registered text commands", delete_after=3)
 
 
 print("starting bot")
