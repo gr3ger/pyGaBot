bb08c8e71fa0b03f510f87b20c0039a0bef77e46
gr3ger <gr3ger@gmail.com>
2021-06-10 21:02:35 +0200

    try to remedy weird twitch announce error

--- twitch_cog.py
+++ twitch_cog.py
@@ -1,22 +1,31 @@
-import asyncio
+import datetime
 import http
 import json
 import sys
+import time
 from http.client import HTTPSConnection
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 import settings
+
+
+def current_time_string():
+    return datetime.datetime.utcfromtimestamp(time.time()).strftime('%H:%M:%S')
 
 
 class TwitchCog(commands.Cog, name="Twitch"):
     def __init__(self, bot):
         self.bot = bot
-        self.task = None
 
-        if self.task is None and settings.read_option(settings.KEY_TWITCH_INTEGRATION, "False") == "True":
-            self.task = asyncio.create_task(self.poll_thread())
+        self.was_previously_online = False
+        self.nextUpdateAllowedAt = 0
+
+        self.poll_thread.start()
+
+    def cog_unload(self):
+        self.poll_thread.cancel()
 
     def get_twitch_user_by_name(self, usernames):
         try:
@@ -34,7 +43,7 @@ class TwitchCog(commands.Cog, name="Twitch"):
                 'client-id': settings.TWITCH_CLIENT_ID})
             response = connection.getresponse()
 
-            print("Twitch: {}: {} {}".format(req, response.status, response.reason))
+            print("[{}] Twitch: {}: {} {}".format(current_time_string(), req, response.status, response.reason))
             if response.status == 401:
                 self.get_access_token()
                 return self.get_twitch_user_by_name(usernames)
@@ -92,33 +101,32 @@ class TwitchCog(commands.Cog, name="Twitch"):
             print(e, file=sys.stderr)
             return e
 
+    @tasks.loop(seconds=settings.TWITCH_POLL_RATE)
     async def poll_thread(self):
-        was_previously_online = False
-        while True:
-            notification_sent = False
+        if settings.read_option(settings.KEY_TWITCH_INTEGRATION, "False") == "True" \
+                and time.time() > self.nextUpdateAllowedAt:
             try:
                 result_json = self.get_streams(settings.read_option(settings.KEY_TWITCH_CHANNEL, ""))
                 is_online = False
                 for stream in result_json["data"]:
                     if stream["user_name"] == settings.read_option(settings.KEY_TWITCH_CHANNEL, ""):
                         is_online = True
-                        if not was_previously_online:
-                            notification_sent = True
+                        if not self.was_previously_online:
                             await self.send_message_to_channel(
                                 settings.TWITCH_ANNOUNCEMENT_MESSAGE.format(
                                     streamer=stream['user_name'],
                                     stream_link="https://twitch.tv/" + stream['user_name'],
                                     stream_description=stream['title']),
                                 int(settings.read_option(settings.KEY_ANNOUNCEMENT_CHANNEL_TWITCH, 0)))
-                print("Twitch: isOnline: {}, wasPreviouslyOnline: {}".format(is_online, was_previously_online))
-                was_previously_online = is_online
+                            self.nextUpdateAllowedAt = time.time() + settings.TWITCH_POLL_COOLDOWN_MINUTES * 60
+                        break
+                print("[{}] Twitch: isOnline: {}, wasPreviouslyOnline: {}".format(current_time_string(), is_online,
+                                                                                  self.was_previously_online))
+                self.was_previously_online = is_online
             except Exception as e:
                 print(e)
-            if notification_sent:
-                print("Twitch: Notification sent, going for a long sleep")
-                await asyncio.sleep(settings.TWITCH_POLL_COOLDOWN_MINUTES * 60)
-            else:
-                await asyncio.sleep(settings.TWITCH_POLL_RATE)
+        else:
+            print("[{}] Waiting to be allowed to check again".format(current_time_string()))
 
     async def send_message_to_channel(self, string, channel_id: int):
         print("Sending announcement to channel {}".format(channel_id))
@@ -128,10 +136,6 @@ class TwitchCog(commands.Cog, name="Twitch"):
     @commands.command()
     @commands.has_any_role("Mods", "Admin")
     async def disabletwitch(self, ctx):
-        """Stop sending twitch updates"""
-        self.task.cancel()
-        self.task = None
-
         settings.write_option(settings.KEY_TWITCH_INTEGRATION, "False")
         await ctx.send("Twitch integration disabled")
 
@@ -156,9 +160,6 @@ class TwitchCog(commands.Cog, name="Twitch"):
                 await ctx.send(
                     "Successfully set the announcement channel to: {}, I will post here when {} comes online.".format(
                         ctx.message.channel.name, twitch_username))
-
-                if self.task is None:
-                    self.task = asyncio.create_task(self.poll_thread())
             except IndexError:
                 await ctx.send("Could not find user {}".format(twitch_username))
             except Exception as e:
